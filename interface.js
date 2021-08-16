81a0481c957480e65eb916408618e19150680635
Brett Karlson <brettjkarlson@gmail.com>
2021-08-16 08:40:40 +0100

    both API requests working

--- interface.js
+++ interface.js
@@ -15,16 +15,33 @@ document.addEventListener("DOMContentLoaded", () => {
         let id = results[i].id
         let webTitle = results[i].webTitle
         let imgSrc = results[i].fields.thumbnail
+
+        createImgElement(imgSrc)
+        createLiElement(id, webTitle)
       }
     })
   }
 
   displayHeadLines()
 
+  function createLiElement(id, webTitle) {
+    li = document.createElement("li")
+    li.innerHTML = `<a href="#${id}"> ${webTitle} </a><br>`
+    document.querySelector("#headlines").append(li)
+  }
+
 function getSummary(webUrl, callback) {
      fetch(`http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=${webUrl}`)
     .then(response => response.json())
     .then(data => callback(data.sentences))
+    .catch(error => console.error(error))
+}
+
+
+function createImgElement(imgSrc) {
+  img = document.createElement("img")
+  img.setAttribute("src", `${imgSrc}`)
+  document.querySelector("#headlines").append(img)
 }
 
 window.addEventListener("hashchange", () => {
@@ -33,6 +50,12 @@ window.addEventListener("hashchange", () => {
   getSummary(webUrl, (sentences) => {
     let summary = sentences.join(" ")
     document.querySelector("#summary").innerText = summary
+  })
+
+  document.querySelector("#back").addEventListener("click", () => {
+    location.href = location.href.split("#")[0]
+    document.querySelector("#summary").innerText = ''
+    displayHeadLines()
   })
 })
 })
